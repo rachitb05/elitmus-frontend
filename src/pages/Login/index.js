d6488c815c7639f562a659f72c5638a54e730ac1
rachitb05 <86082020+rachitb05@users.noreply.github.com>
2023-04-17 05:12:08 +0530

    update

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -15,6 +15,7 @@ function Login() {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
         },
         withCredentials: true,
       })
